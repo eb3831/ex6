2abfc43278d15ad96c4e6b17c75dfb93dce69a19
eb3831 <eb3831@bs.amalnet.k12.il>
2024-01-08 11:47:33 +0200

    update 2

--- app/src/main/java/com/example/ex6/MainActivity.java
+++ app/src/main/java/com/example/ex6/MainActivity.java
@@ -22,7 +22,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn = findViewById(R.id.btn);
-
         iv = findViewById(R.id.iv);
 
 
